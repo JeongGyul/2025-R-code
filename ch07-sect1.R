26ae05aee13705ff6e07d8f8fd34e4e0801b95b4
JeongGyul <jjascd@naver.com>
2025-07-21 11:43:04 +0900

    edit ch07-sect1.R

--- ch07-sect1.R
+++ ch07-sect1.R
@@ -108,6 +108,122 @@ boxplot.stats(st$Income)$out
 
 
 
+# code 7-9
+v1 <- c(1,7,6,8,4,2,3)
+order(v1)
+v1 <- sort(v1)
+v1
+v2 <- sort(v1, decreasing=T)
+v2
+
+# code 7-10
+head(iris)
+order(iris$Sepal.Length)
+iris[order(iris$Sepal.Length),]
+iris[order(iris$Sepal.Length, decreasing=T),]
+iris.new <- iris[order(iris$Sepal.Length),]
+head(iris.new)
+iris[order(iris$Species, -iris$Petal.Length, decreasing=T),]
+
+
+# code 7-11
+sp <- split(iris, iris$Species)
+sp
+summary(sp)
+str(sp)
+class(sp)
+typeof(sp)
+mode(sp)
+
+sp$setosa
+sp$virginica
+sp$versicolor
+sp[[1]]
+sp[[2]]
+sp[[3]]
+
+# code 7-12
+subset(iris, Species == "setosa")
+subset(iris, Sepal.Length > 7.5)
+subset(iris, Sepal.Length > 5.1 & Sepal.Width > 3.9)
+subset(iris, Sepal.Length > 7.6, select=c(Petal.Length, Petal.Width))
+
+iris[c(1,2)]
+iris[colnames(iris)[1:2]]
+iris[colnames(iris)[3:4]]
+
+
+# 다음은 동일
+subset(iris, Sepal.Length > 7.6, select=c(Petal.Length, Petal.Width))
+subset(iris, Sepal.Length > 7.6)[colnames(iris)[3:4]]
+
+
+# code 7-13
+x <- 1:100
+y <- sample(x, size=10, replace = FALSE)
+y
+
+# code 7-14
+idx <- sample(1:nrow(iris), size=50, replace = FALSE)
+idx
+iris.50 <- iris[idx,]
+dim(iris.50)
+head(iris.50)
+View(iris.50)
+
+# code 7-15
+sample(1:20, size=5)
+sample(1:20, size=5)
+sample(1:20, size=5)
+
+set.seed(100)
+sample(1:20, size=5)
+set.seed(100)
+sample(1:20, size=5)
+set.seed(100)
+sample(1:20, size=5)
+
+
+if (!require(dplyr)) {
+  install.packages("dplyr")
+}
+library(dplyr)
+
+??sample_n
+sample_n(iris, 10)
+sample_frac(iris) # 전체
+sample_frac(iris, .1) # 10%
+sample_frac(iris, .2) # 20%
+
+# code 7-16
+combn(1:5, 3)
+
+x = c("red", "green", "blue", "black", "white")
+com <- combn(x, 2)
+com
+str(com)
+class(com)
+typeof(com)
+ncol(com)
+
+for(i in 1:ncol(com)) {           # 조합을 출력
+  cat(com[,i], "\n")
+}
+
+# choose() 조합의 수(종류)를 반환하는 함수
+choose(5, 3)
+choose(5, 4)
+choose(5, 2)
+
+
+
+
+
+
+
+
+
+
 
 
 
